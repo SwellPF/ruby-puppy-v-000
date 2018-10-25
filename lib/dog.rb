eb797b2dd223cde4118afccce04a633b94701e95
Paul Fell-Cinoman <swellpf@gmail.com>
2018-10-25 16:41:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,14 @@ class Dog
   attr_accessor :name
   @@ALL=[]
   
-def initialize(name)  
-  @name = name
-  @@ALL << self
-end
+  def initialize(name)  
+    @name = name
+    @@ALL << self
+  end
 
-def self.all
-  @ALL.each do |dog|
-    puts "#{dog}"
-end
+  def self.all
+    @ALL.each do |dog|
+      puts "#{dog}"
+    end
+  end
 end
